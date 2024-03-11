ec7b72ab7b8149fb455aba6b7e080b0d9dd03414
louis <louis@lemoncloud.io>
2024-03-11 15:05:36 +0900

    chore: add commitlint ignores

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+    extends: ['@commitlint/config-conventional'],
+    ignores: [
+        (message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message),
+        (message) => message.includes('chore(release)')
+    ],
+};
